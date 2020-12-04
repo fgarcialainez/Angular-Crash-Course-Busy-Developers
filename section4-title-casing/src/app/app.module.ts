4894b55318d561e51a4e690b76551a8348ece7e5
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-12-04 20:34:04 +0100

    Completed assignment 3 of the section 4 of the course (Displaying Data and Handling Events)

---
+++ section4-title-casing/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TitleCasePipe } from './title-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TitleCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
