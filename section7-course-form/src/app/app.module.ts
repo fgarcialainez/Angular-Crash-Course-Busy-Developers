c869289b94106211bc32b112839d446290a8fba8
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-12-06 09:16:51 +0100

    Completed assignment 6 of the section 7 of the course (Template-driven Forms)

---
+++ section7-course-form/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewCourseFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
