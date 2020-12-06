16241d1e5a2d736f09a540e907501b675018fc52
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-12-06 17:30:23 +0100

    Completed assignment 7 of the section 8 of the course (Reactive Forms)

---
+++ section8-change-password-form/src/app/change-password/change-password.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PasswordValidators } from './change-password.validators';
+
+@Component({
+  selector: 'change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.css']
+})
+export class ChangePasswordComponent {
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      oldPassword: ['', Validators.required, PasswordValidators.validOldPassword],
+      newPassword: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    }, { validators: PasswordValidators.passwordsShouldMatch });
+  }
+
+  get oldPassword() { return this.form.get('oldPassword') }
+  get newPassword() { return this.form.get('newPassword') }
+  get confirmPassword() { return this.form.get('confirmPassword') }
+
+}
