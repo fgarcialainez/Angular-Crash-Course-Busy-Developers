54c682c115cc30f21a7f55c9cd9314c24abd0717
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-12-12 08:54:02 +0100

    Completed code for all the remaining lectures of the section

--- section10-routes-and-navigation/src/app/github-profile/github-profile.component.ts
+++ section10-routes-and-navigation/src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap
+      .subscribe(params => {
+        let id = +params.get('id')
+        console.log(id);
+      })
+  }
+
+  submit() {
+    this.router.navigate(['/followers'], {
+      queryParams: { page: 1, order: 'newest' }
+    })
   }
 
 }
